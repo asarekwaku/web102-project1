b4a8095822a492c86670b59ed4b247bde6c50127
Kwakuofosujuniorasare <kwakuofosujuniorasare@gmail.com>
2024-09-23 23:45:57 -0400

    Add Card component with basic structure and styling

---
+++ Code Hub/src/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './Card.css'
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <img className="card-image" src={props.image} alt={props.alt} />
+      <h2 className="card-title">{props.title}</h2>
+      <p className="card-description">{props.description}</p>
+      <a className="card-link" href={props.link}>
+        Visit Page</a>
+  </div>
+  );
+};
+
+export default Card
